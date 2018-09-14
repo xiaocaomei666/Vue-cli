50beaf445f10ffd824f30096b48a102719bef060
吴茂霞 <xiaocaomei666@163.com>
2018-09-14 14:22:03 +0800

    Vue-cli

---
+++ src/components/componentA.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="component-a">
+    <span>我是a组件的内容</span>
+    <span>{{changeName}}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'componentA',
+  props: {
+    changeName: {
+      type: String,
+      default: '我是默认的'
+    }
+  },
+  data () {
+    return {
+      msg: 'Welcome to a'
+    }
+  },
+  watch: {
+    changeName (newV, oldV) {
+      console.log(newV, oldV)
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
